fa09ac1fda042c4a1849d33eea3f995be7465d9f
wti <wti@users.noreply.github.com>
2023-09-26 15:10:56 -0700

    DriverTest: support error tests DRAFT not failing

--- Tests/clutchTests/DriverTests.swift
+++ Tests/clutchTests/DriverTests.swift
@@ -8,8 +8,12 @@ import struct SystemPackage.FilePath
 final class DriverTests: XCTestCase {
   typealias Scenario = ClutchCommandScenario
   typealias KnownCalls = KnownSystemCalls
-  typealias Check = KnownSystemCallFixtures.ScenarioCheck
+  typealias ScenarioCase = KnownSystemCallFixtures.ScenarioCase
+  typealias Check = KnownSystemCallFixtures.Check
+  typealias SceneCheck = KnownSystemCallFixtures.ScenarioCheck
   typealias CallCheck = RecordSystemCalls.IndexFuncCall
+  typealias ClutchErr = ClutchDriver.Problem.ErrParts
+  typealias UserAsk = DriverConfig.UserAsk
 
   let fixtures = KnownSystemCallFixtures()
   let dataToStdout = false
@@ -18,28 +22,70 @@ final class DriverTests: XCTestCase {
     let cases = Scenario.allCases
     //let cases: [Scenario] = [.nest(.peers)]
     for scenario in cases {
-      let (calls, scenarioArgs, checks) = fixtures.newScenario(scenario)
-      for (error, srcLoc) in calls.internalErrors {
+      let sc =  fixtures.newScenario(scenario)
+      try await runTest(scenario, sc)
+    }
+  }
+
+  public func testSyntaxErr() async throws {
+    let scenario: Scenario = .nest(.dir)
+    let sc =  fixtures.newScenario(scenario)
+    guard let prefix = UserAsk.nestDir.prefix else {
+      preconditionFailure("no prefix on UserAsk.nestDir")
+    }
+    let args = sc.args.with(args: ["\(prefix)NOT_FOUND"])
+    try await runTest(.nest(.dir), .sc(sc.calls, args, [.clutchErr("syntax")]))
+  }
+
+  func runTest(_ scenario: Scenario, _ test: ScenarioCase) async throws {
+    for (error, srcLoc) in test.calls.internalErrors {
         XCTFail(error, file: srcLoc.file, line: srcLoc.line)
-      }
-      if calls.internalErrors.isEmpty {
-        let name = scenario.name
-        let args = scenarioArgs.args
-        let recordCalls = try await run(label: name, calls: calls, args: args)
-        check(scenario, calls: recordCalls, checks: checks)
+    }
+    guard test.calls.internalErrors.isEmpty else {
+      return
+    }
+    let (calls, args, checks) = (test.calls, test.args.args, test.checks)
+    let name = calls.scenarioName
+    do {
+      let recordCalls = try await run(label: name, calls: calls, args: args)
+      check(scenario, checks: checks, calls: recordCalls)
+    } catch  {
+      if let err = error as? ClutchDriver.Problem.ErrParts {
+        check(scenario, checks: checks, clutchError: err)
+      } else {
+        check(scenario, checks: checks, error: error)
       }
     }
   }
 
-  func check(_ test: Scenario, calls: RecordSystemCalls, checks: [Check]) {
+  func check(_ test: Scenario, checks: [Check], calls: RecordSystemCalls) {
     let found = calls.renders
-    func match(_ check: Check, _ callCheck: CallCheck) -> Bool {
+    func match(_ check: SceneCheck, _ callCheck: CallCheck) -> Bool {
       check.call == callCheck.funct
         && callCheck.call.contains(check.match)
     }
-    for check in checks {
+    for check in checks.scenarios {
       if nil == found.first(where: { match(check, $0) }) {
         XCTFail("\(test) expected \(check)")
+      }
+    }
+    for error in checks.errors {
+      XCTFail("\(test) missing error \(error.label)")
+    }
+  }
+  func check(_ test: Scenario, checks: [Check], clutchError: ClutchErr) {
+    checkError(test, checks: checks, error: "\(clutchError)")
+  }
+  func check(_ test: Scenario, checks: [Check], error: Error) {
+    checkError(test, checks: checks, error: "\(error)")
+  }
+  func checkError(_ test: Scenario, checks: [Check], error: String) {
+    for check in checks.scenarios {
+      XCTFail("\(test) expected \(check)")
+    }
+    for check in checks.errors {
+      if !error.contains(check.match) {
+        XCTFail("\(test)\nexp error: \(check.label)\ngot error: \(error)")
       }
     }
   }
