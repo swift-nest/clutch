462b5a721441f92731b08dbe7a88738d32cb5b47
wti <wti@users.noreply.github.com>
2023-09-26 18:24:43 -0700

    DriverConfig.findNest: add error to FoundNest (report dups when found)

--- Sources/clutch/DriverConfig/DriverConfig.swift
+++ Sources/clutch/DriverConfig/DriverConfig.swift
@@ -1,7 +1,8 @@
-typealias FoundNest = (nest: NestItem, name: String)
+typealias FoundNest = (nest: NestItem, name: String, error: String?)
 
 /// Namespace for configuration
 public enum DriverConfig {
+  typealias EnvName = PeerNest.EnvName
   /// Find nest path, searching:
   ///
   /// - path: env.path
@@ -12,6 +13,7 @@ public enum DriverConfig {
     _ nestNameFromScript: String?,
     using clutchSystem: SystemCalls
   ) -> FoundNest? {
+    var error: String? = nil
     let seeker = FileItemSeeker(systemCalls: clutchSystem)
     func envVar(_ key: PeerNest.EnvName) -> String? {
       clutchSystem.seekEnv(key)
@@ -20,10 +22,14 @@ public enum DriverConfig {
     if let path = envVar(.NEST_PATH) {
       let nest = seeker.seekDir(.nest, path)
       if nest.status.isDir,
-        let name = nest.filePath.lastComponent?.string,
-        !name.isEmpty
+         let name = nest.filePath.lastComponent?.string,
+         !name.isEmpty
       {
-        return (nest, name)
+        if let scName = nestNameFromScript, !scName.isEmpty {
+          error = "Using \(EnvName.NEST_PATH.key) but script nest is \(scName)"
+          return (nest, name, error)
+        }
+        return (nest, name, nil)
       }
     }
 
@@ -39,11 +45,14 @@ public enum DriverConfig {
       }
       let base = seeker.seekDir(.nest, path)
       if base.status.isDir {
-        for name in names {
+        for (i, name) in names.enumerated() {
           let nestPath = base.filePath.appending(name).string
           let nest = seeker.seekDir(.nest, nestPath)
           if nest.status.isDir {
-            return (nest, name)
+            if i > 0 {
+              error = "ignoring names \(names[0..<i])"
+            }
+            return (nest, name, error)
           }
         }
       }
@@ -51,16 +60,27 @@ public enum DriverConfig {
     }
 
     // Base directory candidates are NEST_BASE, HOME + (RPATH | git)
-    if let result = tryBaseDir(envVar(.NEST_BASE)) {
-      return result
+    var baseDirs = [String]()
+    if let rpath = envVar(.NEST_BASE) {
+      baseDirs.append(rpath)
     }
     if let homePath = envVar(.HOME),
-      let home = seeker.seekDirOrNil(.HOME, homePath)
-    {
-      for rpath in notEmpty([envVar(.NEST_HOME_RPATH), "git"]) {
-        if let result = tryBaseDir(home.filePath.appending(rpath).string) {
+       let home = seeker.seekDirOrNil(.HOME, homePath) {
+      if let rpath = envVar(.NEST_HOME_RPATH) {
+        baseDirs.append(home.filePath.appending(rpath).string)
+      }
+      baseDirs.append(home.filePath.appending("git").string)
+    }
+    for (i, baseDir) in baseDirs.enumerated() {
+      if let result = tryBaseDir(baseDir) {
+        if (0 == i) {
           return result
         }
+        let dirErr = "After trying base dirs \(baseDirs[0..<i])"
+        guard let prior = result.error else {
+          return (result.nest, result.name, dirErr)
+        }
+        return (result.nest, result.name, "\(prior); \(dirErr)")
       }
     }
     return nil
