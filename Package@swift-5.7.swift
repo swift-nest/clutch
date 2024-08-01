3e793dc1bb882a2fbfc388259a0e0d411b3602eb
wti <wti@users.noreply.github.com>
2024-08-01 14:56:19 -0700

    Swift-6 support Packages

--- Package@swift-5.7.swift
+++ Package@swift-5.7.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.7
 
 import PackageDescription
 
@@ -6,7 +6,7 @@ let name = "clutch"
 let clatch = "clatch"
 let package = Package(
   name: name,
-  platforms: [.macOS(.v12)],
+  platforms: [ .macOS(.v12) ],
   products: [
     .executable(name: name, targets: [name]),
     .executable(name: clatch, targets: [clatch]),
@@ -24,29 +24,28 @@ let package = Package(
   targets: [
     .target(
       name: "\(name)Lib",
-      dependencies: [.product(name: "Script", package: "Shwift")],
+      dependencies: [ .product(name: "Script", package: "Shwift") ],
       path: "Sources/\(name)"
     ),
-    .executableTarget(  // RUN:clutch
+    .executableTarget( // RUN:clutch
       name: name,
-      dependencies: [.target(name: "\(name)Lib")],
+      dependencies: [ .target(name: "\(name)Lib") ],
       path: "Sources/\(name)-tool"
     ),
     .executableTarget(
       name: "ClutchAP",
-      dependencies: [.target(name: "\(name)Lib")]
+      dependencies: [ .target(name: "\(name)Lib") ]
     ),
     .executableTarget(
       name: clatch,
-      dependencies: [.product(name: "Script", package: "Shwift")]
+      dependencies: [ .product(name: "Script", package: "Shwift") ]
     ),
     .testTarget(
       name: "\(name)Tests",
       dependencies: [
-        .target(name: "\(name)Lib"),
-        .product(name: "Atomics", package: "swift-atomics")
+      .target(name: "\(name)Lib"),
+      .product(name: "Atomics", package: "swift-atomics")
       ]
-    ),
-  ],
-  swiftLanguageVersions: [.v6]
+    )
+  ]
 )
