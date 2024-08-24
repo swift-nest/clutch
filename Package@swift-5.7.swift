cc7e8fd214e4be0dc041e67252b201e9ce5ce2d4
wti <wti@users.noreply.github.com>
2024-08-24 00:42:37 -0700

    SansShwift: 5.x deps: omit Shwift, add SystemPackage & ArgumentParser

--- Package@swift-5.7.swift
+++ Package@swift-5.7.swift
@@ -5,6 +5,15 @@ import PackageDescription
 let name = "clutch"
 let clatch = "clatch"
 let clutchArgParser = "ClutchAP"
+func apple(
+  _ package: String,
+  _ version: String
+) -> PackageDescription.Package.Dependency {
+  .package(
+    url: "https://github.com/apple/\(package)",
+    from: Version(stringLiteral: version)
+  )
+}
 let package = Package(
   name: name,
   platforms: [ .macOS(.v12) ],
@@ -14,19 +23,16 @@ let package = Package(
     .executable(name: clutchArgParser, targets: [clutchArgParser]),
   ],
   dependencies: [
-    .package(
-      url: "https://github.com/GeorgeLyon/Shwift.git",
-      from: Version(stringLiteral: "3.1.1")
-    ),
-    .package(
-      url: "https://github.com/apple/swift-atomics.git",
-      from: Version(stringLiteral: "1.2.0")
-    ),
+    apple("swift-atomics", "1.2.0"),
+    apple("swift-system", "1.2.2"),
+    apple("swift-argument-parser", "1.5.0"),
   ],
   targets: [
     .target(
       name: "\(name)Lib",
-      dependencies: [ .product(name: "Script", package: "Shwift") ],
+      dependencies: [
+        .product(name: "SystemPackage", package: "swift-system"),
+      ],
       path: "Sources/\(name)"
     ),
     .executableTarget( // RUN:clutch
@@ -36,11 +42,18 @@ let package = Package(
     ),
     .executableTarget(
       name: clutchArgParser,
-      dependencies: [ .target(name: "\(name)Lib") ]
+      dependencies: [
+        .target(name: "\(name)Lib"),
+        .product(
+          name: "ArgumentParser",
+          package: "swift-argument-parser"),
+      ]
     ),
     .executableTarget(
       name: clatch,
-      dependencies: [ .product(name: "Script", package: "Shwift") ]
+      dependencies: [
+        .product(name: "SystemPackage", package: "swift-system"),
+      ]
     ),
     .testTarget(
       name: "\(name)Tests",
